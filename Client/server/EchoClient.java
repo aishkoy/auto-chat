d774e14b39994554cee037aa359c87fec75f5347
aishkou <aisha.orozbekova@alatoo.edu.kg>
2025-02-13 23:22:29 +0600

    feat: Added EchoClient and EchoServer with factory methods for instantiation

--- Client/server/EchoClient.java
+++ Client/server/EchoClient.java
@@ -1,4 +1,15 @@
 package server;
 
 public class EchoClient {
+    private final int port;
+    private final String host;
+
+    private EchoClient(int port, String host) {
+        this.port = port;
+        this.host = host;
+    }
+
+    public static EchoClient connectTo(int port, String host) {
+        return new EchoClient(port, host);
+    }
 }
--- Server/server/EchoServer.java
+++ Server/server/EchoServer.java
@@ -1,4 +1,13 @@
 package server;
 
 public class EchoServer {
+    private final int port;
+
+    private EchoServer(int port) {
+        this.port = port;
+    }
+
+    public static EchoServer bindToPort(int port) {
+        return new EchoServer(port);
+    }
 }
