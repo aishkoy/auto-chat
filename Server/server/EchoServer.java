f37502bf0dd992dc26fdcc0bb959927a6144103c
aishkou <aisha.orozbekova@alatoo.edu.kg>
2025-02-13 23:29:30 +0600

    feat: added run method to EchoServer for handling client connections

--- Server/server/EchoServer.java
+++ Server/server/EchoServer.java
@@ -1,5 +1,9 @@
 package server;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class EchoServer {
     private final int port;
 
@@ -9,5 +13,16 @@ public class EchoServer {
 
     public static EchoServer bindToPort(int port) {
         return new EchoServer(port);
+    }
+
+    public void run(){
+        try(ServerSocket serverSocket = new ServerSocket(port)){
+            try(Socket socket = serverSocket.accept()){
+                handle(socket);
+            }
+        } catch (IOException e){
+            System.out.println("Вероятно этот порт уже занят");
+            e.printStackTrace();
+        }
     }
 }
